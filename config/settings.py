e3d72baea184d6873a7ed8ff7831a666ffaf1d80
shglp1 <alem00213@gmail.com>
2026-05-03 13:58:32 +0300

    Configure project for PythonAnywhere deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e13kv$hy7c%cw+y+&9$28vn*5x=-@pi=2toj*i-+dtz5pl7h9k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shglp1.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
